7de4f4efb1888b487968c69b4b378a1b8681bf1b
Teru <uneteru@protonmail.com>
2021-12-20 14:16:16 +0100

    adding navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react'
+import Navbar from './Navbar';
 
 class App extends Component {
     render() {
         return(
-            <div className='text-center' style={{
-                fontSize: '30px'
-                }}>
-                <h3>Welcome to Debank</h3>
-                <img src='./bank.png'></img>
-            </div>
+            <Navbar />
         )
     }
 }
