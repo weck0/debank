7e44091f0814012ff9cd55981ea20ef1e23f0762
Teru <uneteru@protonmail.com>
2021-12-20 21:36:05 +0100

    setting up main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import tether from '../tether.png';
 
 class Main extends Component {
     render() {
@@ -17,8 +18,35 @@ class Main extends Component {
                             <td>RWD</td>
                         </tr>
                     </tbody>
-                </table>
 
+                </table>
+                <div className='card mb-2' style={{opacity: '.9'}}>
+                    <form className='mb-3'>
+                        <div style={{borderSpace: '0 1em'}}>
+                            <label className='float: left' style={{marginLeft:'15px', fontWeight:'bold'}}>Stake Tokens</label>
+                            <span className='float-right' style={{marginRight:'8px'}}>
+                                Balance :
+                            </span>
+                            <div className='input-group nb-4'>
+                                <input
+                                type='text'
+                                placeholder='0'
+                                required />
+                                <div className='input-group-open'>
+                                    <div className='input-grouped-text'>
+                                        <img alt='tether' src={tether} height='32' />
+                                        &nbsp;&nbsp;&nbsp;USDT
+                                    </div>
+                                </div>
+                            </div>
+                            <button type='submit' className='btn btn-primary btn-lg btn-block'>DEPOSIT</button>
+                        </div>
+                    </form>
+                    <button className='btn btn-primary btn-lg btn-block'>WITHDRAW</button>
+                    <div className='card-body text-center' style={{color:'blue'}}>
+                        AIRDROP
+                    </div>
+                </div>
             </div>
         )
     }
