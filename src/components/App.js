ed867fa4b2b7c83d4956c58fcf26286eaf7fbb45
Teru <uneteru@protonmail.com>
2021-12-20 14:04:29 +0100

    setting up react

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+    render() {
+        return(
+            <div className='text-center' style={{
+                fontSize: '30px'
+                }}>
+                <h3>Welcome to Debank</h3>
+                <img src='./bank.png'></img>
+            </div>
+        )
+    }
+}
+
+export default App;
