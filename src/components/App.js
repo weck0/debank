d1ddcc3ca4f4b5b4163520d2cad73f92fe5f047e
Teru <uneteru@protonmail.com>
2021-12-20 15:46:33 +0100

    getting eth account

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import './App.css';
 import Navbar from './Navbar';
 import Web3 from 'web3';
+import Tether from '../truffle_abis/Tether.json';
 
 class App extends Component {
 
@@ -24,14 +25,20 @@ class App extends Component {
     async loadBlockchainData() {
         const web3 = window.web3
         const account = await web3.eth.getAccounts()
-        console.log(account)
+        this.setState({account: account[0]})
         
     }
 
     constructor(props) {
         super(props)
         this.state = { 
-            account: '0x0'
+            tether: {},
+            rwd: {},
+            decentralBank: {},
+            tetherBalance: '0',
+            rwdBalance: '0',
+            stakingBalance: '0',
+            loading: true
         }
     }
 
